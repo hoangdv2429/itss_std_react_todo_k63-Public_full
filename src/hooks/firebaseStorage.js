67a41d8e576790bda33ee76bfa756d9e9ed3b1ac
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2022-06-30 03:20:13 +0700

    temp fix to save to firestore

--- src/hooks/firebaseStorage.js
+++ src/hooks/firebaseStorage.js
@@ -11,32 +11,33 @@ export default function useFirebaseStorage() {
     }, [items]);
 
     const getItems = async () => {
-        const _items = await getFirebaseItems();
-        setItems(_items);
+        const res = await getFirebaseItems();
+        if (res.success)
+            setItems(res.items);
     };
 
-    const addItem = async item => {
+    const addItem = async (item) => {
         const newItem = { text: item.text, done: item.done };
         await addFirebaseItem(newItem);
         setItems([...items, newItem]);
     };
 
-    const updateItem = async (changedItem, isCheck) => {
-        const newItem = { ...changedItem, done: isCheck };
-        await updateFirebaseItem(newItem, changedItem.id);
+    const updateItem = async (checked) => {
+        const changes = { done: !checked.done };
+        await updateFirebaseItem(changes, checked.id);
         const newItems = items.map((item) => {
-            if (item.id === changedItem.id) {
-                item.done = isCheck;
+            if (item.id === checked.id) {
+                item = { ...item, changes };
             }
             return item;
-        })
+        });
         setItems(newItems);
-    }
+    };
 
     const clearItems = () => {
-        items.map(item => {
+        items.map((item) => {
             clearFirebaseItem(item);
-        })
+        });
         setItems([]);
     };
 
